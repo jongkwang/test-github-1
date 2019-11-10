0f6e3f851a0b84433d5b8d95e1487a8b71f4b6b5
SpongeBob <xcpc7@naver.com>
2019-11-10 13:34:31 +0900

    Add a sum function()

--- Calculrator.java
+++ Calculrator.java
@@ -1,7 +1,11 @@
 package com.mycompany.test;
 
 public class Calculator {
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
